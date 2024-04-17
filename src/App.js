6731046fe0cae966984a406347952fe85e7d4e7b
julius-tamale <juliotamex@proton.me>
2024-04-17 06:01:24 +0300

    Woodex react project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from "./Components/Header";
+import SideBar from "./Components/SideBar";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
+
 function App() {
   return (
     <div>
-      TEST
+      <Header />
+      <SideBar/>
+      <Main />
+      <Footer />
     </div>
   );
 }
